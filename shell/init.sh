d7d48e61593ddbb115b1242af29e655b1902a302
Peter Hodge <peterhodge@example.com>
2016-09-22 10:55:24 +1000

    shell/: tidy up

--- shell/init.sh
+++ shell/init.sh
@@ -1,25 +1,21 @@
-# TODO: only when running in zsh!
-if type add-zsh-hook &> /dev/null; then
-    if which jerjerrod &> /dev/null; then
-        __wantclear=
-        jerjerrod_clearcache() {
-            jerjerrod_clearcache_now
-            expanded="$3"
-            if echo "$expanded" | grep '^\(git\|hg\) ' &> /dev/null; then
-                echo WANTED
-                __wantclear=1
-            fi
-        }
-        add-zsh-hook preexec jerjerrod_clearcache
+if type add-zsh-hook &> /dev/null && which jerjerrod &> /dev/null; then
+    __wantclear=
+    jerjerrod_clearcache() {
+        jerjerrod_clearcache_now
+        expanded="$3"
+        if echo "$expanded" | grep '^\(git\|hg\) ' &> /dev/null; then
+            __wantclear=1
+        fi
+    }
+    add-zsh-hook preexec jerjerrod_clearcache
 
-        jerjerrod_clearcache_now() {
-            if [ -n "$__wantclear" ]; then
-                jerjerrod clearcache --local "$PWD"
-                echo "CLEARING"
-                __wantclear=
-            fi
-        }
+    jerjerrod_clearcache_now() {
+        if [ -n "$__wantclear" ]; then
+            jerjerrod clearcache --local "$PWD"
+            echo "CLEARING"
+            __wantclear=
+        fi
+    }
 
-        export PS1='`jerjerrod_clearcache_now`'"$PS1"
-    fi
+    export PS1='`jerjerrod_clearcache_now`'"$PS1"
 fi
