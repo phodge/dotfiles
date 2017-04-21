eee5e0e7f6b6ad58892f73610008654b3d0dacc0
Peter Hodge <peterhodge@example.com>
2017-04-22 05:54:05 +1000

    powerline, tmux: Don't indent entire functions

--- powerline/HOMELY.py
+++ powerline/HOMELY.py
@@ -9,79 +9,81 @@ from HOMELY import HERE, HOME, powerline_path, wantjerjerrod, wantpowerline
 
 @section
 def powerline():
-    if wantpowerline():
-        mkdir('~/.config')
-        mkdir('~/.config/powerline')
-        paths = [
-            "%s/config_files" % powerline_path(),
-            "%s/powerline" % HERE,
-            "%s/.config/powerline" % HOME,
-        ]
-        lineinfile('~/.shellrc',
-                   'export POWERLINE_CONFIG_PATHS=%s' % ":".join(paths),
-                   where=WHERE_END)
+    if not wantpowerline():
+        return
 
-        # ask the user what colour prefs they would like and put it in
-        # ~/.config/powerline/colors.sh
-        colourfile = os.path.join(HOME, '.config', 'powerline', 'colours.sh')
-        load = False
-        defaults = dict(
-            bg="gray1",
-            fg1="white",
-            fg2="gray6",
-        )
-        if not os.path.exists(colourfile):
-            if yesno(None, 'Select base colours now?', True, noprompt=False):
-                # load available colours from colors.json
-                with open("%s/config_files/colors.json" % powerline_path()) as f:
-                    import simplejson
-                    colors = simplejson.load(f)
-                with open(colourfile, 'w') as f:
-                    f.write("# Set the 3 variables using colour names from below.\n")
-                    f.write("# WARNING! If you misspell a colour your powerline may not work!\n")
-                    f.write("#\n")
-                    f.write("# primary background colour\n")
-                    f.write("bg=%(bg)s\n" % defaults)
-                    f.write("# foreground colour for highlighted tab\n")
-                    f.write("fg1=%(fg1)s\n" % defaults)
-                    f.write("# foreground colour for other tabs\n")
-                    f.write("fg2=%(fg2)s\n" % defaults)
-                    f.write("# possible colours:\n")
-                    for name in sorted(colors.get("colors", {})):
-                        f.write("#   %s\n" % name)
-                system(['vim', colourfile], stdout="TTY")
-                load = True
-        else:
+    mkdir('~/.config')
+    mkdir('~/.config/powerline')
+    paths = [
+        "%s/config_files" % powerline_path(),
+        "%s/powerline" % HERE,
+        "%s/.config/powerline" % HOME,
+    ]
+    lineinfile('~/.shellrc',
+                'export POWERLINE_CONFIG_PATHS=%s' % ":".join(paths),
+                where=WHERE_END)
+
+    # ask the user what colour prefs they would like and put it in
+    # ~/.config/powerline/colors.sh
+    colourfile = os.path.join(HOME, '.config', 'powerline', 'colours.sh')
+    load = False
+    defaults = dict(
+        bg="gray1",
+        fg1="white",
+        fg2="gray6",
+    )
+    if not os.path.exists(colourfile):
+        if yesno(None, 'Select base colours now?', True, noprompt=False):
+            # load available colours from colors.json
+            with open("%s/config_files/colors.json" % powerline_path()) as f:
+                import simplejson
+                colors = simplejson.load(f)
+            with open(colourfile, 'w') as f:
+                f.write("# Set the 3 variables using colour names from below.\n")
+                f.write("# WARNING! If you misspell a colour your powerline may not work!\n")
+                f.write("#\n")
+                f.write("# primary background colour\n")
+                f.write("bg=%(bg)s\n" % defaults)
+                f.write("# foreground colour for highlighted tab\n")
+                f.write("fg1=%(fg1)s\n" % defaults)
+                f.write("# foreground colour for other tabs\n")
+                f.write("fg2=%(fg2)s\n" % defaults)
+                f.write("# possible colours:\n")
+                for name in sorted(colors.get("colors", {})):
+                    f.write("#   %s\n" % name)
+            system(['vim', colourfile], stdout="TTY")
             load = True
-            if yesno(None, 'Select base colours now?', False, noprompt=False):
-                system(['vim', colourfile], stdout="TTY")
+    else:
+        load = True
+        if yesno(None, 'Select base colours now?', False, noprompt=False):
+            system(['vim', colourfile], stdout="TTY")
 
-        colourset = defaults
-        if load:
-            with open(colourfile, 'r') as f:
-                for line in [l.rstrip() for l in f]:
-                    if len(line) and not line.startswith('#'):
-                        import pprint
-                        print('line = ' + pprint.pformat(line))  # noqa TODO
-                        name, val = line.split('=')
-                        colourset[name] = val
-        data = {}
-        data["groups"] = {
-            "window:current":       {"bg": colourset["bg"],  "fg": colourset["fg1"], "attrs": []},
-            "window_name":          {"bg": colourset["bg"],  "fg": colourset["fg1"], "attrs": ["bold"]},  # noqa
-            "session:prefix":       {"bg": colourset["bg"],  "fg": "gray90", "attrs": ["bold"]},
-            "active_window_status": {"fg": colourset["fg2"], "bg": "gray0", "attrs": []},
-            "hostname":             {"bg": colourset["bg"],  "fg": "gray90", "attrs": []},
-        }
-        # write out a colorscheme override for tmux using our powerline colours
-        mkdir('~/.config')
-        mkdir('~/.config/powerline')
-        mkdir('~/.config/powerline/colorschemes')
-        mkdir('~/.config/powerline/colorschemes/tmux')
-        import simplejson
-        dumped = simplejson.dumps(data)
-        with writefile('~/.config/powerline/colorschemes/tmux/default.json') as f:
-            f.write(dumped)
+    colourset = defaults
+    if load:
+        with open(colourfile, 'r') as f:
+            for line in [l.rstrip() for l in f]:
+                if len(line) and not line.startswith('#'):
+                    import pprint
+                    print('line = ' + pprint.pformat(line))  # noqa TODO
+                    name, val = line.split('=')
+                    colourset[name] = val
+    data = {}
+    data["groups"] = {
+        "window:current":       {"bg": colourset["bg"],  "fg": colourset["fg1"], "attrs": []},
+        "window_name":          {"bg": colourset["bg"],  "fg": colourset["fg1"], "attrs": ["bold"]},  # noqa
+        "session:prefix":       {"bg": colourset["bg"],  "fg": "gray90", "attrs": ["bold"]},
+        "active_window_status": {"fg": colourset["fg2"], "bg": "gray0", "attrs": []},
+        "hostname":             {"bg": colourset["bg"],  "fg": "gray90", "attrs": []},
+    }
+    # write out a colorscheme override for tmux using our powerline colours
+    mkdir('~/.config')
+    mkdir('~/.config/powerline')
+    mkdir('~/.config/powerline/colorschemes')
+    mkdir('~/.config/powerline/colorschemes/tmux')
+    import simplejson
+    dumped = simplejson.dumps(data)
+    with writefile('~/.config/powerline/colorschemes/tmux/default.json') as f:
+        f.write(dumped)
 
 
 @section
--- tmux/HOMELY.py
+++ tmux/HOMELY.py
@@ -17,76 +17,80 @@ def _wanttmux():
 
 @section
 def tmux_config():
-    if _wanttmux():
-        # needed for tmux
-        if wantpowerline():
-            pipinstall('powerline-status', ['pip3'])
+    if not _wanttmux():
+        return
 
-        tmux_plugins = yesno('install_tmux_plugins',
-                             'Install TPM and use tmux plugins?',
-                             wantfull())
+    # needed for tmux
+    if wantpowerline():
+        pipinstall('powerline-status', ['pip3'])
 
-        if tmux_plugins:
-            mkdir('~/.tmux')
-            mkdir('~/.tmux/plugins')
-            tpm = InstallFromSource('https://github.com/tmux-plugins/tpm',
-                                    '~/.tmux/plugins/tpm')
-            tpm.select_branch('master')
-            run(tpm)
+    tmux_plugins = yesno('install_tmux_plugins',
+                            'Install TPM and use tmux plugins?',
+                            wantfull())
 
-        # what to put in tmux config?
-        wildcards = {"DOTFILES": HERE, "HOME": HOME}
-        lines = []
-        # add our dotfiles python folder to PYTHONPATH before restarting the powerline daemon
-        #lines.append("set-environment PYTHONPATH '%(DOTFILES)s/python'")
-        if wantpowerline():
-            wildcards["POWERLINE"] = powerline_path()
-            lines.extend([
-                # always replace the daemon on startup, so that re-sourcing the
-                # tmux conf always loads updated python modules
-                'run-shell "PYTHONPATH=%(DOTFILES)s/python powerline-daemon --replace -q"',
-                'source "%(POWERLINE)s/bindings/tmux/powerline.conf"',
-            ])
-        if tmux_plugins:
-            lines.append('source "%(DOTFILES)s/tmux/plugins.conf"')
-            # FIXME: the only way to determine if TPM installed correctly is to
-            # press `[PREFIX]`, `I` to do a plugin install
-        lines.append('source "%(DOTFILES)s/tmux/tmux.conf"')
-        lines = [l % wildcards for l in lines]
-        blockinfile('~/.tmux.conf',
-                    lines,
-                    '# start of automated tmux config',
-                    '# end of automated tmux config',
-                    where=WHERE_TOP)
+    if tmux_plugins:
+        mkdir('~/.tmux')
+        mkdir('~/.tmux/plugins')
+        tpm = InstallFromSource('https://github.com/tmux-plugins/tpm',
+                                '~/.tmux/plugins/tpm')
+        tpm.select_branch('master')
+        run(tpm)
+
+    # what to put in tmux config?
+    wildcards = {"DOTFILES": HERE, "HOME": HOME}
+    lines = []
+    # add our dotfiles python folder to PYTHONPATH before restarting the powerline daemon
+    #lines.append("set-environment PYTHONPATH '%(DOTFILES)s/python'")
+    if wantpowerline():
+        wildcards["POWERLINE"] = powerline_path()
+        lines.extend([
+            # always replace the daemon on startup, so that re-sourcing the
+            # tmux conf always loads updated python modules
+            'run-shell "PYTHONPATH=%(DOTFILES)s/python powerline-daemon --replace -q"',
+            'source "%(POWERLINE)s/bindings/tmux/powerline.conf"',
+        ])
+    if tmux_plugins:
+        lines.append('source "%(DOTFILES)s/tmux/plugins.conf"')
+        # FIXME: the only way to determine if TPM installed correctly is to
+        # press `[PREFIX]`, `I` to do a plugin install
+    lines.append('source "%(DOTFILES)s/tmux/tmux.conf"')
+    lines = [l % wildcards for l in lines]
+    blockinfile('~/.tmux.conf',
+                lines,
+                '# start of automated tmux config',
+                '# end of automated tmux config',
+                where=WHERE_TOP)
 
 
 # install or compile tmux
 @section
 def tmux_install():
-    if _wanttmux():
-        if yesno('own_tmux', 'Compile tmux from source?', None):
-            # FIXME: compiling tmux from source like this requires libevent ...
-            # how do we make sure that that library has been installed?
-            tmux = InstallFromSource('https://github.com/tmux/tmux.git',
-                                     '~/src/tmux.git')
-            tmux.select_tag('2.3')
-            tmux.compile_cmd([
-                ['sh', 'autogen.sh'],
-                ['./configure'],
-                ['make'],
-            ])
-            tmux.symlink('tmux', '~/bin/tmux')
-            tmux.symlink('tmux.1', '~/man/man1/tmux.1')
-            run(tmux)
-        else:
-            try:
-                # install tmux using brew or apt-get ...
-                installpkg('tmux')
-            except:
-                print("-" * 50)
-                print("Compiling `tmux` failed - do you need to install automake or gcc?")  # noqa
-                print("-" * 50)
-                raise
+    if not _wanttmux():
+        return
+
+    if yesno('own_tmux', 'Compile tmux from source?', None):
+        # FIXME: compiling tmux from source like this requires libevent ...
+        # how do we make sure that that library has been installed?
+        tmux = InstallFromSource('https://github.com/tmux/tmux.git',
+                                    '~/src/tmux.git')
+        tmux.select_tag('2.3')
+        tmux.compile_cmd([
+            ['sh', 'autogen.sh'],
+            ['./configure'],
+            ['make'],
+        ])
+        tmux.symlink('tmux', '~/bin/tmux')
+        tmux.symlink('tmux.1', '~/man/man1/tmux.1')
+        run(tmux)
+    else:
+        try:
+            # install tmux using brew or apt-get ...
+            installpkg('tmux')
+        except:
+            print("-" * 50)
+            print("Compiling `tmux` failed - do you need to install automake or gcc?")  # noqa
+            print("-" * 50)
+            raise
 
 
 class TmuxCustomMode(object):
