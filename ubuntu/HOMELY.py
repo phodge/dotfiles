84c5472c1926c6a2429e7a5e15c5a86b475bd5a3
Peter Hodge <peter.hodge84@gmail.com>
2018-12-10 10:47:20 +1000

    ubuntu: swap caps/escape; set keyboard repeat rate

---
+++ ubuntu/HOMELY.py
@@ -0,0 +1,43 @@
+from homely.general import section
+from homely.system import execute
+from homely.ui import yesno
+
+from HOMELY import wantfull
+
+
+@section
+def ubuntu_swap_caps_escape():
+    if not wantfull():
+        return
+
+    if not yesno('ubuntu_swap_caps_escape', 'Ubuntu: Swap caps/escape using dconf-editor?'):
+        return
+
+    cmd = ['dconf', 'read', '/org/gnome/desktop/input-sources/xkb-options']
+    current = execute(cmd, stdout=True)[1].strip()
+    if b"'caps:swapescape'" in current:
+        # already done
+        return
+
+    if current == b'' or current == b'[]':
+        new_value = "['caps:swapescape']"
+    else:
+        raise Exception("TODO: don't know how to modify xkb-options value")  # noqa
+
+    execute(['dconf', 'write', '/org/gnome/desktop/input-sources/xkb-options', new_value])
+
+
+@section
+def ubuntu_key_repeat_rate():
+    if not wantfull():
+        return
+
+    if not yesno('ubuntu_set_repeat_rate', 'Ubuntu: Set keyboard repeat rate?'):
+        return
+
+    new_values = [
+        ('repeat-interval', 'uint32 15'),
+        ('delay',           'uint32 210'),
+    ]
+    for key, value in new_values:
+        execute(['gsettings', 'set', 'org.gnome.desktop.peripherals.keyboard', key, value])
