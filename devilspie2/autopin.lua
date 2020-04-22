09e7b0f389cba6d273dc84c602ff6cde5fc1e4d6
Peter Hodge <peter.hodge84@gmail.com>
2020-04-22 14:51:25 +1000

    devilspie2: auto pin Microsoft Teams

--- devilspie2/autopin.lua
+++ devilspie2/autopin.lua
@@ -2,9 +2,12 @@ local window_class = get_window_class()
 local app_name = get_application_name()
 if (window_class == "Mattermost"
     or window_class == "Spotify"
+    or window_class == 'Microsoft Teams - Preview'
     or app_name == "Spotify"
     or app_name == 'Rhythmbox'
 ) then
     print(string.format("Pinning %s/%s", window_class, app_name))
     pin_window()
+else
+    print(string.format("Not pinning %s/%s", window_class, app_name))
 end
