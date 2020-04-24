699b78950258fdf8e1eaf9531e09409098689e36
Peter Hodge <peter.hodge84@gmail.com>
2020-04-24 12:01:35 +1000

    vim: improved retest.sh skeleton

--- vim/skeletons/retest.sh
+++ vim/skeletons/retest.sh
@@ -2,5 +2,12 @@
 clear
 date
 
-do_something \
-    && echo YAY || echo FAIL
+fail() { local z="$?"; echo -e "\e[01;31m[$(date +'%H:%M:%S%P')] FAIL[$z]: $@\e[0m" >&2; exit 1; }
+win() { echo -e "\e[01;32m[$(date +'%H:%M:%S%P')] $@\e[0m" >&2; }
+say() { echo -e "\e[01;34m[$(date +'%H:%M:%S%P')] $@\e[0m" >&2; }
+
+say "about to do something"
+
+echo winning || fail did not win
+
+win retest.sh OK
