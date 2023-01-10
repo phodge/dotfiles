9ff23e3dd64221baaf57f32f40e13624d60f704a
Peter Hodge <peter.hodge84@gmail.com>
2023-01-10 22:38:05 +1000

    Rework jerjerrod installation to use a dedicated virtualenv

--- jerjerrod/HOMELY.py
+++ jerjerrod/HOMELY.py
@@ -1,10 +1,12 @@
-from homely.general import run, section, haveexecutable, WHERE_TOP
-from homely.files import lineinfile
+from pathlib import Path
+
+from homely.files import lineinfile, mkdir, symlink
+from homely.general import haveexecutable, run, section
 from homely.install import InstallFromSource
+from homely.system import execute
 from homely.ui import yesno
 
-from HOMELY import HERE, jerjerrod_addline, wantjerjerrod, IS_OSX
-
+from HOMELY import HERE, IS_OSX, jerjerrod_addline, wantjerjerrod
 
 jerjerrod_clear_cache_in_shell = wantjerjerrod() and yesno(
     'jerjerrod_clear_cache_in_shell',
@@ -24,29 +26,54 @@ def jerjerrod_system_flags():
     )
 
 
+VENV_PATH = Path('~/playground-6/virtualenvs/jerjerrod.venv').expanduser()
+
+
+@section(enabled=wantjerjerrod(), interval='4w')
+def jerjerrod_virtualenv_upgrade():
+    if VENV_PATH.exists():
+        execute(
+            ['python3', '-m', 'venv', '--upgrade-deps', str(VENV_PATH)]
+        )
+
+
 @section(enabled=wantjerjerrod())
 def jerjerrod_install():
+    # create a virtualenv for jerjerrod
+    mkdir(str(VENV_PATH.parent.parent))
+    mkdir(str(VENV_PATH.parent))
+    if not VENV_PATH.exists():
+        # create the virtualenv
+        execute(
+            ['python3', '-m', 'venv', '--upgrade-deps', str(VENV_PATH)]
+        )
+
+    # create a symlink of jerjerrod to the virtualenv
+    symlink(str(VENV_PATH / 'bin/jerjerrod'), '~/bin/jerjerrod')
+
     # NOTE: only install jerjerrod where we've installed powerline
     install_commands = [
-        ['pip3', 'install', '--user', '-e', '.'],
+        [str(VENV_PATH / 'bin/pip'), 'install', '-e', '.'],
     ]
 
     if IS_OSX and haveexecutable('/usr/bin/pip3'):
         # XXX: this version of pip3 is too old to understand how to install in
-        # ---editable mode with just a pyproject.toml, so we need to just
-        # install it as-is
+        # --editable mode with just a pyproject.toml, so we need to just
+        # install it as-is so that powerline has access to jerjerrod libs as
+        # well
         install_commands.append(
             ['/usr/bin/pip3', 'install', '--user', '.'],
         )
 
     # install from source!
     inst = InstallFromSource('https://github.com/phodge/jerjerrod.git',
-                             '~/src/jerjerrod.git')
+                             '~/playground-6/jerjerrod.git')
     inst.select_branch('master')
     # TODO: if you uninstall jerjerrod, this won't actually reinstalll it :-(
     inst.compile_cmd(install_commands)
     run(inst)
 
+    jerjerrod_addline('PROJECT', '~/playground-6/*.git')
     jerjerrod_addline('PROJECT', '~/src/*.git')
     jerjerrod_addline('PROJECT', '~/src/*.hg')
     jerjerrod_addline('PROJECT', HERE)
