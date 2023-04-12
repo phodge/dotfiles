28064f96a9ec1012f24794ea0f82cf59f267d772
Peter Hodge <peter.hodge84@gmail.com>
2023-04-12 10:06:57 +1000

    Update vim config for neovim 0.9

--- vim_lsp_config.lua
+++ vim_lsp_config.lua
@@ -2,8 +2,8 @@
 
 require("lspconfig").tsserver.setup({
     on_attach = function(client, bufnr)
-        client.resolved_capabilities.document_formatting = false
-        client.resolved_capabilities.document_range_formatting = false
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
         local ts_utils = require("nvim-lsp-ts-utils")
         ts_utils.setup({})
         ts_utils.setup_client(client)
