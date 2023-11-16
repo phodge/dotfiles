d44d28b652610affe6939323643ba5fe8fd5484b
Peter Hodge <peter.hodge84@gmail.com>
2023-11-17 05:27:56 +1000

    Treesitter / Ex-Machinae notes & experimentation

---
+++ examples/syntax_test.py
@@ -0,0 +1,29 @@
+# TODO: DOTFILES001: use this file to compare Python syntax w/ TreeSitter
+# import from stdlib
+import json
+import os.path
+from typing import TYPE_CHECKING, List
+
+# other imports
+
+
+
+# primitive values
+FOO = None
+BAR = False
+SOME_INT = 5
+SOME_FLOAT = 5.6
+SOME_STR = 'hello world'
+OTHER_STR = "hello everyone\n"
+
+def cheese():
+    foo = False
+
+cheese = bar[0]
+cheese = bar[0:1]
+
+# data structures
+list1 = [ 1, 2, 3]
+list2 = [[2], [3]]
+list3 = ['a', 'b', ]
+dict1 = (({'a': 1, 'b': 2}, ))
