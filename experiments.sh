ae59a838db09bc33d12006100d5a5fb3b5079688
Peter Hodge <peter.hodge84@gmail.com>
2025-03-06 10:57:43 +1000

    Placeholder experiments.sh for me to add devices

---
+++ experiments.sh
@@ -0,0 +1,12 @@
+# I. DEVICE IDS
+#
+# Each device needs its own unique ID. Create a unique ID for this device by
+# running the following command:
+#
+#   test -e $HOME/.config/experiments_uuid || uuidgen > $HOME/.config/experiments_uuid
+#
+# Then add a variable and function for your new machine below:
+DEEPCOOL2_UUID=acc4f383-d2f3-411c-a84d-68e7891b9007
+MACITRON_UUID=todo_here
+SPARK_UUID=todo_here
+OCTOMACM2_UUID=todo_here
