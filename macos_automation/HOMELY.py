ea28cb518ed092bd6576d917b8833e25a0f6a2ff
Peter Hodge <peter.hodge84@gmail.com>
2026-02-14 21:11:55 +1000

    Manual steps for installing/configuring BTT on macOS

--- macos_automation/HOMELY.py
+++ macos_automation/HOMELY.py
@@ -1,5 +1,5 @@
 from textwrap import dedent
-from HOMELY import section_macos, manual_step
+from HOMELY import section_macos, manual_step, allow_installing_stuff
 
 
 def _get_macos_keybindings():
@@ -69,4 +69,84 @@ def macos_standard_keybindings():
             '''
         ),
         undoable=False,
+    )
+
+
+@section_macos(enabled=allow_installing_stuff)
+def macos_better_touch_tool():
+    new_bindings = _get_macos_keybindings()
+    open_terminal_key = new_bindings["OPEN_TERMINAL"][0].macos_human_readable
+    toggle_maximize_key = new_bindings["TOGGLE_MAXIMIZED"][0].macos_human_readable
+
+    manual_step(
+        'macos_better_touch_tool_install',
+        'Install and configure Better Touch Tool',
+        dedent(
+            '''
+            1. Download and install BTT v5.139 from https://folivora.ai/releases/btt5.139-2025012805.zip
+            2. Open BTT and grant screen-recording and accessibility
+               permissions in System Preferences.
+            3. Configure BTT:
+               A) Launch BTT on startup
+               B) Disable automatic update checking (until we get a new license?)
+               C) Disable window snapping
+            '''
+        ),
+        undoable=True,
+    )
+    manual_step(
+        'macos_btt_shortcut_open_terminal',
+        'Add a Better Touch Tool shortcut for OPEN_TERMINAL',
+        dedent(
+            f'''
+            1. Open BTT Configuration
+            2. Add a Keyboard Shortcut
+               Key sequence: {open_terminal_key}
+               Actions:
+               i) Send Shortcut to a Specific App: Alacritty
+                  Shortcut: {open_terminal_key}
+               ii) 'open application': Alacritty
+            '''
+        ),
+        undoable=True,
+    )
+
+    simple_btt_keys = [
+        ("FORCE_MAXIMIZED", "Maximize window"),
+        ("MOVE_WINDOW_NEXT_DISPLAY", "Center Window on Next Monitor"),
+        ("SWITCH_TO_WORKSPACE_LEFT", "Move Left a Space"),
+        ("SWITCH_TO_WORKSPACE_RIGHT", "Move Right a Space"),
+    ]
+
+    for keyname, btt_action in simple_btt_keys:
+        keysequence = new_bindings[keyname][0].macos_human_readable
+        manual_step(
+            f'macos_btt_shortcut_{keyname}',
+            f'Add a Better Touch Tool shortcut for {keyname}',
+            dedent(
+                f'''
+                1. Open BTT Configuration
+                2. Add a Keyboard Shortcut
+                   Key sequence: {keysequence}
+                   Action: {btt_action}
+                '''
+            ),
+            undoable=True,
+        )
+
+    manual_step(
+        'macos_btt_shortcut_maximize_window',
+        'Add a Better Touch Tool shortcut for FORCE_MAXIMIZED',
+        dedent(
+            f'''
+            1. Open BTT Configuration
+            2. Add a Keyboard Shortcut
+               Key sequence: {force_maximize_key}
+               Action: Maximize Window
+            4. Add a Keyboard Shortcut
+               Key sequence: ????
+               Action: Navigating Spaces ... > Move Window One Space/Desktop Left
+            '''
+        ),
+        undoable=True,
     )
